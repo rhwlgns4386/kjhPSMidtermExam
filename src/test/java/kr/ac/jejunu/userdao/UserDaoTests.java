72423e562aa99b2df912b90f837555cca92873ab
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2021-04-23 16:15:02 +0900

    2번

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -15,7 +15,7 @@ public class UserDaoTests {
         Integer id = 1;
         String name = "hulk";
         String password = "1234";
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -28,7 +28,37 @@ public class UserDaoTests {
         user.setName("고지훈");
         user.setPassword("1234");
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
+        userDao.insert(user);
+
+
+        User check = userDao.get(user.getId());
+
+        assertThat(check.getId(),greaterThan(0));
+        assertThat(check.getId(), is(user.getId()));
+        assertThat(check.getName(), is(user.getName()));
+        assertThat(check.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void 한라testGet() throws SQLException, ClassNotFoundException {
+        Integer id = 1;
+        String name = "hulk";
+        String password = "1234";
+        UserDao userDao = new HalaUserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void 한라추가() throws SQLException, ClassNotFoundException {
+        User user=new User();
+        user.setName("고지훈");
+        user.setPassword("1234");
+
+        UserDao userDao = new HalaUserDao();
         userDao.insert(user);
 
 
