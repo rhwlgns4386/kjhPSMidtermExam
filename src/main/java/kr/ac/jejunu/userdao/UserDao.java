50237c9d8f1a7a789b9bed1719972480201ad42f
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2021-04-23 15:35:21 +0900

    첫커밋

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,59 +1,29 @@
 package kr.ac.jejunu.userdao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-
 import java.sql.*;
 
 public class UserDao {
+    public User get(Integer id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
+                , "jeju", "jejupw");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
 
-    private JdbcTemplate jdbcTemplate;
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
 
-    public UserDao(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate=jdbcTemplate;
-    }
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
 
-    public User get(Integer id) throws SQLException {
-        String sql="select * from userinfo where id = ?";
-        return jdbcTemplate.query(sql,rs -> {
-            User user=null;
-            if (rs.next()) {
-                user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-            return user;
-        },id);
-    }
-
-
-    public void insert(User user) throws SQLException {
-        String sql="insert into userinfo(name,password) value (?,?)";
-        Object obj[]=new Object[]{user.getName(),user.getPassword()};
-        KeyHolder keyHolder=new GeneratedKeyHolder();
-        jdbcTemplate.update(con -> {
-            PreparedStatement preparedStatement;
-            preparedStatement =
-                    con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            for (int i = 0; i< obj.length; i++){
-                preparedStatement.setObject(i+1, obj[i]);
-            }
-            return preparedStatement;
-        },keyHolder);
-        user.setId(keyHolder.getKey().intValue());
-    }
-
-    public void update(User user) throws SQLException {
-        String sql="update userinfo set name=?,password=? where id=?";
-        Object obj[]=new Object[]{user.getName(),user.getPassword(),user.getId()};
-        jdbcTemplate.update(sql,obj);
-    }
-
-    public void delete(Integer id) throws SQLException {
-        String sql="delete from userinfo where id=?";
-        Object obj[]=new Object[]{id};
-        jdbcTemplate.update(sql,obj);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //리턴
+        return user;
     }
 }
--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -1,92 +1,23 @@
 package kr.ac.jejunu.userdao;
 
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
-    private static UserDao daoFactory;
-    @BeforeAll
-    public static void setup(){
-        ApplicationContext applicationContext= new AnnotationConfigApplicationContext(DaoFactory.class);
-        daoFactory=applicationContext.getBean("userDao",UserDao.class);
-    }
-
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
         Integer id = 1;
         String name = "hulk";
         String password = "1234";
-
-        UserDao userDao = daoFactory;
+        UserDao userDao = new UserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
-    }
-
-    @Test
-    public void 저장() throws SQLException, ClassNotFoundException {
-        User user=new User();
-
-        String name = "언니...";
-        String password = "힘내...";
-        user.setName(name);
-        user.setPassword(password);
-        UserDao userDao = daoFactory;
-
-        userDao.insert(user);
-
-        User check = userDao.get(user.getId());
-        assertThat(check.getId(), is(user.getId()));
-        assertThat(check.getName(), is(user.getName()));
-        assertThat(check.getPassword(), is(user.getPassword()));
-    }
-
-    @Test
-    public void 업데이트() throws SQLException, ClassNotFoundException {
-        User user=new User();
-        UserDao userDao = daoFactory;
-        String name = "언니...";
-        String password = "힘내...";
-        user.setName(name);
-        user.setPassword(password);
-
-        userDao.insert(user);
-
-        user.setName("누나...");
-        user.setPassword("...힘내");
-        userDao.update(user);
-
-        User check = userDao.get(user.getId());
-        assertThat(check.getId(), is(user.getId()));
-        assertThat(check.getName(), is(user.getName()));
-        assertThat(check.getPassword(), is(user.getPassword()));
-    }
-
-    @Test
-    public void 삭제() throws SQLException, ClassNotFoundException {
-        User user=new User();
-
-        String name = "언니...";
-        String password = "힘내...";
-        user.setName(name);
-        user.setPassword(password);
-        UserDao userDao = daoFactory;
-
-        userDao.insert(user);
-
-        userDao.delete(user.getId());
-        User check = userDao.get(user.getId());
-        assertThat(check,nullValue());
-
     }
 }
